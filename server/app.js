c17452235cf7666cff24a2429f289ae3810b5608
Evan Potter <epotter12@gmail.com>
2016-02-20 04:29:58 -0500

    added express static

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 "use strict";
 var express = require('express');
 var app = express();
-var http = require('http').server(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-    res.sendfile('index.html');
-});
+app.use(express.static(__dirname + "/../client"));
 
 io.on('connection', function(socket){
     console.log('a user connected');
